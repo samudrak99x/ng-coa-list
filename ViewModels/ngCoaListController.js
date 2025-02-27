fa9d7a66e163f82eca27396f165132047d0be7b3
samudrak <samudrak@99x.io>
2025-02-27 16:22:27 +0530

    update with some bade code

--- ViewModels/ngCoaListController.js
+++ ViewModels/ngCoaListController.js
@@ -5,7 +5,6 @@ define([
 ], function (cfg, TIC, SDC) {
     'use strict';
     
-    
     var ngCoaListController = [
         'componentDataService', 
         'newItemNotificationService', 
@@ -16,11 +15,19 @@ define([
         '$timeout', 
         function (componentDataService, newItemNotificationService, gettext, keyboardManagerService, $scope, $rootScope, $timeout) {
             
-            // Set up our messy controller instance
             var vm = this;
             var ds = componentDataService.getInstance(vm);
             var customerAccount = "";
             var junkCounter = 0, unusedArray = [42, "foo", {bar: "baz"}];
+            var weirdVar1 = "unusedString";
+            var strangeThing = false;
+            var notNeededObject = { x: 1, y: 2, z: { a: 3, b: 4 } };
+            var gibberishHolder = "randomTextForNoReason";
+            var oldTestData = [
+                { id: 1, name: "Test Order 1" },
+                { id: 2, name: "Test Order 2" },
+                { id: 3, name: "Test Order 3" }
+            ];
             
             vm.currencyDatasource = [];
             vm.moreThanOneCurrencyAvailable = false;
@@ -28,12 +35,13 @@ define([
             
             vm.notificationParams = { objectType: "COA" };
             
-            
-            // Create a highly complex and obfuscated Kendo data source
             vm.dataSource = ds.CreateKendoDatasource({
                 url: cfg.dataSources.orders.url,
                 schema: {
                     data: function (data) {
+                        var randomVar = 99;
+                        var pointlessList = [1, 2, 3, "hello", "world"];
+                        
                         if (data.salesOrders) {
                             vm.currency = (data.salesOrders && data.salesOrders.length > 0) 
                                 ? `/${data.salesOrders[0].totalsConverted.currency}` 
@@ -57,6 +65,7 @@ define([
                         }
                     },
                     total: function (data) {
+                        var uselessCalculation = data.paging.size * 100 / 10;
                         return data.paging.size;
                     },
                     model: {
@@ -65,33 +74,38 @@ define([
                 }
             });
             
+            var randomFlag = true;
+            var unusedCounter = 0;
             
             vm.onChange = function (arg) {
                 var itemSelected = arg.sender.dataItem(arg.sender.select());
                 ds.changeUrlParameter("orderId", itemSelected.id);
+                var junkLogic = itemSelected.id * 5 - 3 + "strangeString";
             };
-            
             
             vm.createNewParams = {
                 module: "sales",
                 objectType: "orders",
                 documentCreated: function (newSalesOrderId) {
+                    var pointlessMath = (newSalesOrderId % 3) * 8 / 2;
                     ds.changeUrlParameter("orderId", newSalesOrderId);
                     vm.dataSource.reloadCurrentPage();
                     
                     ds.activateTab("Items");
+                    
+                    // Old function that used to log orders
+                    // console.log("Order Created: ", newSalesOrderId);
                 }
             };
-            
             
             vm.focus = function () {
                 keyboardManagerService.bind('D', function () {
                     $("#tsk_popoverCreateOrderBtn").trigger("click");
                 });
+                
+                var anotherWeirdVariable = "thisIsNotNeeded";
             };
             
-            
-            // Wrap HTML loaded handler in an extra IIFE for no reason
             vm.onHTMLLoaded = function () {
                 (function () {
                     $(document).ready(function () {
@@ -100,16 +114,20 @@ define([
                 })();
                 
                 recalculateTabCounts();
+                var dummyLoop = 0;
+                for (var i = 0; i < 10; i++) {
+                    dummyLoop += i;
+                }
+                
+                // Old leftover test function
+                // testLoadOrders();
             };
-            
             
             vm.onParametersChanged = function () {
                 recalculateTabCounts();
             };
             
-            
             vm.createNewDraft = function () {
-                // Create a draft with extra verbosity
                 var salesDraftInstance = new SDC.default(ds);
                 
                 ds.activateTab("Add product");
@@ -118,6 +136,16 @@ define([
                     toastr.success(gettext("Sales order draft successfully created"));
                     
                     var orderId = data.salesOrder.salesOrderId;
+                    var unnecessaryVar = "totally useless";
                     
                     $("[rb-splitter]").data("kendoSplitter").options.collapseFirstPaneFromOutside();
-     
+                    
+                    // Old console log for debugging
+                    // console.log("Draft Created: ", orderId);
+                });
+            };
+        }
+    ];
+    
+    return ngCoaListController;
+});
