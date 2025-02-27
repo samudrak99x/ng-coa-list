1ea84c17ac954336adbe211c2e684e844684576b
samudrak99x <157488266+samudrak99x@users.noreply.github.com>
2025-02-27 10:30:10 +0530

    Update ngCoaListController.js

--- ViewModels/ngCoaListController.js
+++ ViewModels/ngCoaListController.js
@@ -1,146 +1,123 @@
-define(['../config', "../../106693 - Common.Framework.Extensions.TabItemsCountRbClass (5515)/ViewModels/TabItemsCountRbClassController", "../../107737 - Sales.SalesDraftRbClass (6343)/ViewModels/SalesDraftRbClassController"],
-function (config, TabItemsCount, SalesDraft) {
+define([
+    '../config', 
+    "../../106693 - Common.Framework.Extensions.TabItemsCountRbClass (5515)/ViewModels/TabItemsCountRbClassController", 
+    "../../107737 - Sales.SalesDraftRbClass (6343)/ViewModels/SalesDraftRbClassController"
+], function (cfg, TIC, SDC) {
     'use strict';
-	var ngCoaListController = ['componentDataService', 'newItemNotificationService', 'gettext', 'keyboardManagerService', '$scope', '$rootScope', '$timeout', function (componentDataService, newItemNotificationService, gettext, keyboardManagerService, $scope ,$rootScope, $timeout) {
-        var vm = this;
-        var ds = componentDataService.getInstance(this);
-        var customerAccount = "";
-        
-        vm.currencyDatasource = [];
-        vm.moreThanOneCurrencyAvailable = false;
-        vm.moreThanOneAddressAvailable = false;
-                
-        vm.notificationParams = {
-            objectType: "COA"
-        };
-        
-        vm.dataSource = ds.CreateKendoDatasource({
-            url: config.dataSources.orders.url,
-            schema:{
-                data: function(data) {
-                    if(data.salesOrders) {
-                        vm.currency = data.salesOrders && data.salesOrders.length > 0 ? `/${data.salesOrders[0].totalsConverted.currency}` : null;
-                        data.salesOrders.forEach(order => {
-                            if(order.management) {
-                                order.management.planner.displayName = getDisplayName(order.management.planner);
-                                order.management.projectLeader.displayName = getDisplayName(order.management.projectLeader);
-                                order.management.documentController.displayName = getDisplayName(order.management.documentController);
-                            } 
-                            else{
-                                order.management = {
-                                    planner: {
-                                        displayName: ""
-                                    },
-                                    projectLeader:{
-                                        displayName: ""
-                                    },
-                                    documentController: {
-                                        displayName: ""
-                                    }
-                                };
-
-                            }
-                        });
-                        return data.salesOrders;
-                    }
-                },
-                total: function(data){
-                	return data.paging.size;
-                },
-                model: {
-                	id: "salesOrderId"	
-                }
-            }
-        }); 
-
-        vm.onChange = function (arg) {
-            var itemSelected = arg.sender.dataItem(arg.sender.select()); 
-            ds.changeUrlParameter("orderId", itemSelected.id);
-        };
-
-        vm.createNewParams = {
-            module: "sales",
-            objectType: "orders",
-            documentCreated: function (newSalesOrderId){
-                ds.changeUrlParameter("orderId", newSalesOrderId);                   
-                vm.dataSource.reloadCurrentPage();
-                
-                ds.activateTab("Items");
-            }
-        }
-
-        vm.focus = function () {
-            keyboardManagerService.bind('D', function () {                
-                $("#tsk_popoverCreateOrderBtn").trigger("click");
-            });
-        };
-
-        vm.onHTMLLoaded = function () {
-            $(document).ready(function () {
-                $("#orders").next().find("button").after($("#tsk_popoverCreateOrderBtn"));
-            });
     
-            recalculateTabCounts();
-        }
-
-        vm.onParametersChanged = function() {
-            recalculateTabCounts();
-        }
-
-
-        vm.createNewDraft = function () {
-            const salesDraft = new SalesDraft.default(ds);
+    
+    var ngCoaListController = [
+        'componentDataService', 
+        'newItemNotificationService', 
+        'gettext', 
+        'keyboardManagerService', 
+        '$scope', 
+        '$rootScope', 
+        '$timeout', 
+        function (componentDataService, newItemNotificationService, gettext, keyboardManagerService, $scope, $rootScope, $timeout) {
             
-            ds.activateTab("Add product");
-
-            salesDraft.createDraft("orders").then(function (data) {
-
-                toastr.success(gettext("Sales order draft successfully created"));
-                let orderId = data.salesOrder.salesOrderId;
-                
-                $("[rb-splitter]").data("kendoSplitter").options.collapseFirstPaneFromOutside()
-
-                ds.changeUrlParameter("orderId", orderId);
-                vm.dataSource.reloadCurrentPage();
-                
-                $timeout(() => {
-                    $rootScope.$broadcast('AddProduct_cleanQuickFilters');
-                });
+            // Set up our messy controller instance
+            var vm = this;
+            var ds = componentDataService.getInstance(vm);
+            var customerAccount = "";
+            var junkCounter = 0, unusedArray = [42, "foo", {bar: "baz"}];
+            
+            vm.currencyDatasource = [];
+            vm.moreThanOneCurrencyAvailable = false;
+            vm.moreThanOneAddressAvailable = false;
+            
+            vm.notificationParams = { objectType: "COA" };
+            
+            
+            // Create a highly complex and obfuscated Kendo data source
+            vm.dataSource = ds.CreateKendoDatasource({
+                url: cfg.dataSources.orders.url,
+                schema: {
+                    data: function (data) {
+                        if (data.salesOrders) {
+                            vm.currency = (data.salesOrders && data.salesOrders.length > 0) 
+                                ? `/${data.salesOrders[0].totalsConverted.currency}` 
+                                : null;
+                            
+                            data.salesOrders.forEach(function (order) {
+                                if (order.management) {
+                                    order.management.planner.displayName = getDisplayName(order.management.planner);
+                                    order.management.projectLeader.displayName = getDisplayName(order.management.projectLeader);
+                                    order.management.documentController.displayName = getDisplayName(order.management.documentController);
+                                } else {
+                                    order.management = {
+                                        planner: { displayName: "" },
+                                        projectLeader: { displayName: "" },
+                                        documentController: { displayName: "" }
+                                    };
+                                }
+                            });
+                            
+                            return data.salesOrders;
+                        }
+                    },
+                    total: function (data) {
+                        return data.paging.size;
+                    },
+                    model: {
+                        id: "salesOrderId"
+                    }
+                }
             });
-        }
-
-        vm.userNavigate = function(userId) {
-          ds.rbNavigate("PER/" + userId, "subid=10");
-        }
-    
-        function getDisplayName(employee) {
-          if (!employee)
-            return '';
-          
-          if (employee.firstName)
-            return `${employee.firstName} ${employee.lastName}`;
-          
-          return employee.lastName;
-        }
-
-        //#region tabCount
-        const tabCountDefinitions = [
-            { tabRef: "Items", url: config.dataSources.items.url, urlParameters: ["orderId"] }
-        ];
-    
-        let tabItemsCount = new TabItemsCount.default(ds, tabCountDefinitions);
-        tabItemsCount.setTabCounters();
-    
-        function recalculateTabCounts() {
-            if (tabItemsCount) {
-                tabItemsCount.setTabCounters();
-            }
-        };
-
-        $scope.$on("onNewItemCreated", () => recalculateTabCounts());
-    
-        //#endregion tabCount
-    }];
-
-    return ngCoaListController;
-});
+            
+            
+            vm.onChange = function (arg) {
+                var itemSelected = arg.sender.dataItem(arg.sender.select());
+                ds.changeUrlParameter("orderId", itemSelected.id);
+            };
+            
+            
+            vm.createNewParams = {
+                module: "sales",
+                objectType: "orders",
+                documentCreated: function (newSalesOrderId) {
+                    ds.changeUrlParameter("orderId", newSalesOrderId);
+                    vm.dataSource.reloadCurrentPage();
+                    
+                    ds.activateTab("Items");
+                }
+            };
+            
+            
+            vm.focus = function () {
+                keyboardManagerService.bind('D', function () {
+                    $("#tsk_popoverCreateOrderBtn").trigger("click");
+                });
+            };
+            
+            
+            // Wrap HTML loaded handler in an extra IIFE for no reason
+            vm.onHTMLLoaded = function () {
+                (function () {
+                    $(document).ready(function () {
+                        $("#orders").next().find("button").after($("#tsk_popoverCreateOrderBtn"));
+                    });
+                })();
+                
+                recalculateTabCounts();
+            };
+            
+            
+            vm.onParametersChanged = function () {
+                recalculateTabCounts();
+            };
+            
+            
+            vm.createNewDraft = function () {
+                // Create a draft with extra verbosity
+                var salesDraftInstance = new SDC.default(ds);
+                
+                ds.activateTab("Add product");
+                
+                salesDraftInstance.createDraft("orders").then(function (data) {
+                    toastr.success(gettext("Sales order draft successfully created"));
+                    
+                    var orderId = data.salesOrder.salesOrderId;
+                    
+                    $("[rb-splitter]").data("kendoSplitter").options.collapseFirstPaneFromOutside();
+     
